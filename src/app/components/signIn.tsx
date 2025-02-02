2783add9537230ff59ab136de1c63bf6b17c0dd5
Admin <MEY230580@spengergasse.at>
2025-02-02 21:57:18 +0100

    adding Code for SignIn (not usable - there is no API)

---
+++ src/app/components/signIn.tsx
@@ -0,0 +1,98 @@
+import { LockOutlined } from "@mui/icons-material";
+import {
+    Container,
+    CssBaseline,
+    Box,
+    Avatar,
+    Typography,
+    TextField,
+    Button,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+
+export default function SignInPage() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        fetch("API MUSS NOCH ERSTELLT + VERBUNDEN WERDEN!")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("API Response:", data);
+            setUsername(data)
+        })
+            .catch((error) => {
+                console.error("Error retrieving data:", error);
+                setError(error.message);
+            });
+    }, []);
+
+    const handleSignIn = () => {
+        if (!username) {
+            console.log("Wrong Username!");
+        }
+        if (!password) {
+            console.log("Wrong Password!");
+        }
+        setUsername(username);
+    }
+
+    return (
+        <>
+            {error && <p className="flex items-center justify-center  text-lg  text-red-500">⚠ {error} ⚠</p>}
+            <Container maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        mt: 20,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: "primary.light" }}>
+                        <LockOutlined />
+                    </Avatar>
+                    <Typography variant="h5">Login</Typography>
+                    <Box sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Email Address"
+                            name="email"
+                            autoFocus
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="password"
+                            name="password"
+                            label="Password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                            }}
+                        />
+
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            onClick={handleSignIn}
+                        >
+                            Login
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
+        </>
+    );
+}
